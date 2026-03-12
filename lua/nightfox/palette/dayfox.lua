c18865dbc06b752da82548205cd862abada2934f
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-03-12 14:02:12 +0100

    yellow.dim for warnings and this search

--- lua/nightfox/palette/dayfox.lua
+++ lua/nightfox/palette/dayfox.lua
@@ -81,7 +81,7 @@ local function generate_spec(pal)
 
   spec.diag = {
     error = pal.red.base,
-    warn = pal.yellow.base,
+    warn = pal.yellow.dim,
     info = pal.cyan.base,
     hint = pal.green.base,
     ok = pal.green.base,
