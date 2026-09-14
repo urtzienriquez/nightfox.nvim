48f139fe64d9946208737a0b674829d0991360c2
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-09-14 12:57:51 +0200

    MCursor highlight not needed because now it is well defined in neovim

--- lua/nightfox/highlights.lua
+++ lua/nightfox/highlights.lua
@@ -38,7 +38,6 @@ function M.apply(spec, config)
   -- --------------------------------------------------------------------------
 
   hi("Cursor", { fg = spec.bg1, bg = spec.fg1 })
-  hi("MCursor", {fg = spec.diag_bg.warn, bg = spec.diag.warn })
   hi("CursorLine", { bg = spec.bg3 })
   hi("CursorLineNr", { bg = spec.bg0, fg = spec.diag.warn, bold = true })
   hi("CursorLineNrNC", { fg = spec.fg3, bold = true })
